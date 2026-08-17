7c4b4d58dca2d6320b78e4ddd548fba8408e3f21
Anzar-Akhtar <akhtaranzar6@gmail.com>
2026-08-17 20:41:00 +0530

    Added function_with_return chapter

---
+++ chapter 4/function_return_statement/largest.py
@@ -0,0 +1,8 @@
+def largest(a, b):
+    if a > b:
+        return a
+    else:
+        return b
+
+result = largest(10, 20)
+print("The largest number is:", result)
---
+++ chapter 4/function_return_statement/square.py
@@ -0,0 +1,6 @@
+def square(num):
+    return num * num
+
+result = square(4)
+
+print("The square of 4 is:", result)
