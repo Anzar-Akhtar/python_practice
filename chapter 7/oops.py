2d6b48adaddecef9eeea0c2b4e9419ea60aac8cf
Anzar-Akhtar <akhtaranzar6@gmail.com>
2026-09-05 23:23:26 +0530

    Added OOPs folder

---
+++ chapter 7/oops.py
@@ -0,0 +1,58 @@
+# class Car:
+#     pass
+
+
+# car1 = Car()
+# car2 = Car()
+
+# print(car1)
+# print(car2)
+
+
+
+# class Student:
+#     pass
+
+# student1 = Student()
+
+# student1.name = "Anzar"
+# student1.age = 20
+# student1.course = "BCA"
+
+
+# print(student1.name)
+# print(student1.age)
+# print(student1.course)
+
+
+
+# class Car:
+#     pass
+
+# car1 = Car()
+
+# car1.brand = "Brand: Toyota"
+# car1.model = "Model: Fortuner"
+# car1.price = "Price: 4000000"
+
+# print(car1.brand)
+# print(car1.model)
+# print(car1.price)
+
+
+
+
+class Employee:
+    pass
+
+em1 = Employee()
+
+em1.name = "Anzar"
+em1.age = 20
+em1.salary = 30000
+em1.department = "IT"
+
+print(em1.name)
+print(em1.age)
+print(em1.salary)
+print(em1.department)
