2c91d936fde79928c0b59019cc1a330be10cff58
Anzar-Akhtar <akhtaranzar6@gmail.com>
2026-09-06 11:40:18 +0530

    Modified Again

--- chapter 7/init.py
+++ chapter 7/init.py
@@ -1,46 +1,46 @@
-# class Employee:
+class Employee:
 
-#     def __init__(self, name, age, salary, department):
-#         self.name = name
-#         self.age = age
-#         self.salary = salary
-#         self.department = department
+    def __init__(self, name, age, salary, department):
+        self.name = name
+        self.age = age
+        self.salary = salary
+        self.department = department
 
-# em1 = Employee("Anzar", 20, 30000, "IT")
-# em2 = Employee("Atah", 18, 25000, "Finance")
-# em3 = Employee("Aaira", 17, 30000, "Management")
+em1 = Employee("Anzar", 20, 30000, "IT")
+em2 = Employee("Atah", 18, 25000, "Finance")
+em3 = Employee("Aaira", 17, 30000, "Management")
 
-# print("\n==== Employee 1 Details ===")
-# print(em1.name)
-# print(em1.age)
-# print(em1.salary)
-# print(em1.department)
+print("\n==== Employee 1 Details ===")
+print(em1.name)
+print(em1.age)
+print(em1.salary)
+print(em1.department)
 
-# print("\n==== Employee 2 Details ===")
-# print(em2.name)
-# print(em2.age)
-# print(em2.salary)
-# print(em2.department)
+print("\n==== Employee 2 Details ===")
+print(em2.name)
+print(em2.age)
+print(em2.salary)
+print(em2.department)
 
-# print("\n==== Employee 3 Details ===")
-# print(em3.name)
-# print(em3.age)
-# print(em3.salary)
-# print(em3.department)
+print("\n==== Employee 3 Details ===")
+print(em3.name)
+print(em3.age)
+print(em3.salary)
+print(em3.department)
 
 
-# class Student:
+class Student:
 
-#     def __init__(self, name, age, course):
-#         self.name = name
-#         self.age = age
-#         self.course = course
+    def __init__(self, name, age, course):
+        self.name = name
+        self.age = age
+        self.course = course
 
-# stu1 = Student("Anzar", 20, "BCA")
+stu1 = Student("Anzar", 20, "BCA")
 
-# print(stu1.name)
-# print(stu1.age)
-# print(stu1.course)
+print(stu1.name)
+print(stu1.age)
+print(stu1.course)
 
 
 
@@ -73,4 +73,39 @@ print("\n")
 
 car2.show_details()
 car2.start()
-car2.stop()
+# car2.stop()
+
+
+
+
+class Student:
+
+    def __init__(self, name, age, course):
+        self.name = name
+        self.age = age
+        self.course = course
+
+    def show_details(self):
+        print(f"Name: {self.name}")
+        print(f"Age: {self.age}")
+        print(f"Course: {self.course}")
+
+    def study(self):
+        print(f"{self.name} is studying .....")
+
+    def sleep(self):
+        print(f"{self.name} is sleeping .....")
+
+
+student1 = Student("Tony", 28, "B.Tech")
+student2 = Student("Mony", 26, "M.Tech")
+
+student1.show_details()
+student1.study()
+student1.sleep()
+
+print("\n")
+
+student2.show_details()
+student2.study()
+student2.sleep()
