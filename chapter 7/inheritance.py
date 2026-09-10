21f4f446762056312faae2fb9c4ca6fefd52b0e9
Anzar-Akhtar <akhtaranzar6@gmail.com>
2026-09-10 22:26:16 +0530

    Modified file

--- chapter 7/inheritance.py
+++ chapter 7/inheritance.py
@@ -1,49 +1,49 @@
-# class Animal:
+class Animal:
 
-#     def eat(self):
-#         print("Animal is eating.")
+    def eat(self):
+        print("Animal is eating.")
 
-# class Dog(Animal):
-#     pass
+class Dog(Animal):
+    pass
 
-# dog1 = Dog()
-# dog1.eat()
-
-
-# class Vehicle:
-
-#     def start(self):
-#         print("Vehicle is starting.")
+dog1 = Dog()
+dog1.eat()
 
 
-# class Car(Vehicle):
+class Vehicle:
 
-#     def drive(self):
-#         print("Car is driving.")
-
-# car1 = Car()
-
-# car1.start()
-# car1.drive()
+    def start(self):
+        print("Vehicle is starting.")
 
 
-# class Person:
+class Car(Vehicle):
 
-#     def __init__(self, name):
-#         self.name = name
+    def drive(self):
+        print("Car is driving.")
 
-#     def show_name(self):
-#         print(f"Name: {self.name}")
+car1 = Car()
 
-# class Student(Person):
+car1.start()
+car1.drive()
 
-#     def study(self):
-#         print(f"{self.name} is studying.")
 
-# stu1 = Student("Alice")
+class Person:
 
-# stu1.show_name()
-# stu1.study()
+    def __init__(self, name):
+        self.name = name
+
+    def show_name(self):
+        print(f"Name: {self.name}")
+
+class Student(Person):
+
+    def study(self):
+        print(f"{self.name} is studying.")
+
+stu1 = Student("Alice")
+
+stu1.show_name()
+stu1.study()
 
 
 class Animal:
@@ -60,3 +60,21 @@ animal = Dog()
 
 animal.eat()
 animal.bark()
+
+
+
+class Animal:
+
+    def __init__(self, name):
+        self.name = name
+
+class Dog(Animal):
+
+    def __init__(self, name, breed):
+        super().__init__(name)
+        self.breed = breed
+
+dog1 = Dog("Tommy", "German Shepherd")
+
+print(dog1.name)
+print(dog1.breed)
