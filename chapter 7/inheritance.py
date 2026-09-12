5efe8d2036b14ef417f280d847147891f6030486
Anzar-Akhtar <akhtaranzar6@gmail.com>
2026-09-12 21:32:01 +0530

    Removing Comments

--- chapter 7/inheritance.py
+++ chapter 7/inheritance.py
@@ -1,83 +1,83 @@
-# class Animal:
+class Animal:
 
-#     def eat(self):
-#         print("Animal is eating.")
+    def eat(self):
+        print("Animal is eating.")
 
-# class Dog(Animal):
-#     pass
+class Dog(Animal):
+    pass
 
-# dog1 = Dog()
-# dog1.eat()
-
-
-# class Vehicle:
-
-#     def start(self):
-#         print("Vehicle is starting.")
+dog1 = Dog()
+dog1.eat()
 
 
-# class Car(Vehicle):
+class Vehicle:
 
-#     def drive(self):
-#         print("Car is driving.")
-
-# car1 = Car()
-
-# car1.start()
-# car1.drive()
+    def start(self):
+        print("Vehicle is starting.")
 
 
-# class Person:
+class Car(Vehicle):
 
-#     def __init__(self, name):
-#         self.name = name
+    def drive(self):
+        print("Car is driving.")
 
-#     def show_name(self):
-#         print(f"Name: {self.name}")
+car1 = Car()
 
-# class Student(Person):
-
-#     def study(self):
-#         print(f"{self.name} is studying.")
-
-# stu1 = Student("Alice")
-
-# stu1.show_name()
-# stu1.study()
+car1.start()
+car1.drive()
 
 
-# class Animal:
+class Person:
 
-#     def eat(self):
-#         print("Animal is eating.")
+    def __init__(self, name):
+        self.name = name
 
-# class Dog(Animal):
+    def show_name(self):
+        print(f"Name: {self.name}")
 
-#     def bark(self):
-#         print("Dog is barking")
+class Student(Person):
 
-# animal = Dog()
+    def study(self):
+        print(f"{self.name} is studying.")
 
-# animal.eat()
-# animal.bark()
+stu1 = Student("Alice")
+
+stu1.show_name()
+stu1.study()
+
+
+class Animal:
+
+    def eat(self):
+        print("Animal is eating.")
+
+class Dog(Animal):
+
+    def bark(self):
+        print("Dog is barking")
+
+animal = Dog()
+
+animal.eat()
+animal.bark()
 
 
 
-# class Animal:
+class Animal:
 
-#     def __init__(self, name):
-#         self.name = name
+    def __init__(self, name):
+        self.name = name
 
-# class Dog(Animal):
+class Dog(Animal):
 
-#     def __init__(self, name, breed):
-#         super().__init__(name)
-#         self.breed = breed
+    def __init__(self, name, breed):
+        super().__init__(name)
+        self.breed = breed
 
-# dog1 = Dog("Tommy", "German Shepherd")
+dog1 = Dog("Tommy", "German Shepherd")
 
-# print(dog1.name)
-# print(dog1.breed)
+print(dog1.name)
+print(dog1.breed)
 
 
 
