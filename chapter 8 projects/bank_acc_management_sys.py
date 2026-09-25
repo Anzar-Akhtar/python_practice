ae73a45de887e5e83186705f956ce8d96c35f244
Anzar-Akhtar <akhtaranzar6@gmail.com>
2026-09-25 20:01:01 +0530

    Added new features

--- chapter 8 projects/bank_acc_management_sys.py
+++ chapter 8 projects/bank_acc_management_sys.py
@@ -22,7 +22,118 @@ def create_acc():
     print(accounts)
 
 def view_all_acc():
-    pass
+    print("\n==== VIEW ALL ACCOUNTS ====\n")
+
+    if len(accounts) == 0:
+        print("No Accounts Available!")
+
+    else:
+        for account in accounts:
+            print("Account NO: ", account["acc_no"])
+            print("Name: ", account["name"])
+            print("Age: ", account["age"])
+            print("Account Tyoe: ", account["acc_type"])
+            print("Balance: ", account["balance"])
+
+def search_acc():
+    print("\n==== SEARCH YOUR ACCOUNT ====\n")
+
+    search_acc_no = int(input("Enter Your Account No.: "))
+
+    if len(accounts) == 0:
+        print("No Accounts Available!")
+
+    else:
+        found = False
+        for account in accounts:
+            if (search_acc_no == account["acc_no"]):
+                print("Account NO: ", account["acc_no"])
+                print("Name: ", account["name"])
+                print("Age: ", account["age"])
+                print("Account Tyoe: ", account["acc_type"])
+                print("Balance: ", account["balance"])
+
+                print("\n==== ACCOUNT FOUND SUCCESSFULLY ====\n")
+                found = True
+        if found == False:
+            print("Account Not Found!")
+
+def deposit_money():
+    print("\n==== DEPOSIT MONEY ====\n")
+
+    acc_no = int(input("Enter Your Account No. to Deposit Money: "))
+
+    if len(accounts) == 0:
+        print("No Account Available!")
+
+    else:
+        found = False
+        for account in accounts:
+            if(acc_no == account["acc_no"]):
+                mon_dep = float(input("Enter Money to Deposit: "))
+
+                if mon_dep == 0:
+                    print("Deposit Money can't be zero")
+
+                elif mon_dep < 0:
+                    print("Deposit Money can't be negative")
+
+                else:
+                    account["balance"] += mon_dep
+                    print("\n==== BALANCE UPDATED ====\n")
+                found = True
+
+        if found == False:
+            print("Account Not found!")
+
+def withdrawl_money():
+    print("\n==== WITHDRAWL MONEY ====\n")
+
+    acc_no = int(input("Enter Your Account No. to Withdrawl Money: "))
+
+    if len(accounts) == 0:
+        print("No Account Available!")
+
+    else:
+        found = False
+        for account in accounts:
+            if(acc_no == account["acc_no"]):
+                with_mon = float(input("Enter the Amount you wants to Withdrawl: "))
+
+                if with_mon == 0:
+                    print("Withdrawl Money can't be zero!")
+
+                elif with_mon < 0:
+                    print("Withdrawl Money can't be Negative")
+
+                elif with_mon > account["balance"]:
+                    print("Insufficient Balance! Please Enter Valid Money")
+
+                else:
+                    account["balance"] -= with_mon
+                    print("\n==== AMOUNT WITHDRAWL ====\n")
+                found = True
+
+        if found == False:
+            print("Account Not Found!")
+
+def check_bal():
+    print("\n==== CHECK BALANCE ====\n")
+
+    search_bal = int(input("Enter Your Account No. to check Balance: "))
+
+    if len(accounts) == 0:
+        print("No Account Available!")
+
+    else:
+        found = False
+        for account in accounts:
+            if(search_bal == account["acc_no"]):
+                print("Your Account Balance is: ", account["balance"])
+                found = True
+
+        if found == False:
+            print("Account Not found!")
 
 while True:
     print("\n==== BANK ACCOUNT MANAGEMENT SYSTEM ====\n")
@@ -39,5 +150,29 @@ while True:
 
     choice = input("Enter Your Choice: ")
 
-    if choice == "9":
+    if choice == "1":
+        create_acc()
+
+    elif choice == "2":
+        view_all_acc()
+
+    elif choice == "3":
+        search_acc()
+
+    elif choice == "4":
+        deposit_money()
+
+    elif choice == "5":
+        withdrawl_money()
+
+    elif choice == "6":
+        check_bal()
+
+    elif choice == "7":
+        pass
+
+    elif choice == "8":
+        pass
+
+    elif choice == "9":
         break
